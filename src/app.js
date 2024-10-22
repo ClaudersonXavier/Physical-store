b2c4b01a22ffb2d577c28f8f151291c7547b8db3
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2024-10-22 10:58:35 -0300

    Criação servidor local e teste com winston

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const loggers_js_1 = __importDefault(require("./utils/loggers.js"));
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    loggers_js_1.default.info(`Acessando a raiz do servidor`);
+    res.status(200).send('Estou funcionando');
+});
+app.listen(PORT, () => {
+    loggers_js_1.default.info(`Servidor está rodando em http://localhost:${PORT}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+import logger from './utils/loggers.js' 
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: any, res: {
+    status: any; send: (arg0: string) => void; 
+}) => {
+    logger.info(`Acessando a raiz do servidor`);
+    res.status(200).send('Estou funcionando');
+});
+
+app.listen(PORT, () => {
+    logger.info(`Servidor está rodando em http://localhost:${PORT}`);
+});
